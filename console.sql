a3b83064be1baf6c0f3023eff0bb2b63bde9762d
Oleksandr-Zabo <zoleksandr06@gmail.com>
2025-02-05 21:07:00 +0200

    hw had done

--- console.sql
+++ console.sql
@@ -1,52 +1,91 @@
-﻿create database TestDB;
+﻿create database Academy;
 go
-use TestDB;
+use Academy;
+
+--create tables
 go
-create table Users
+create table Groups
 (
-    Id int primary key identity (1,1),
-    Name nvarchar(50) NOT NULL,
-    Surname nvarchar(50) NOT NULL
+    Id int primary key identity (1,1) not null,
+    Name nvarchar(10) NOT NULL UNIQUE check (LEN(Name) > 0),
+    Rating int not null check (Rating >= 0 and Rating <= 5),
+    Year int not null check (Year >= 1 and Year <= 5)
 );
-go create table Account
-   (
-       Id int primary key identity (1,1),
-       Login nvarchar(50) NOT NULL,
-       Password nvarchar(16) NOT NULL,
-       UserId int NOT NULL,
-       CONSTRAINT FK_Users FOREIGN KEY (UserId) REFERENCES Users(Id)
-   );
 
 go
-
-insert into Users (Name, Surname) values
-                                      ('John', 'Doe'),
-                                      ('Jane', 'Doe'),
-                                      ('Alice', 'Smith'),
-                                      ('Bob', 'Smith'),
-                                      ('Charlie', 'Brown'),
-                                      ('Daisy', 'Brown');
+create table Departments
+(
+    Id int primary key identity (1,1) not null,
+    Financing money not null check (Financing >= 0) default 0,
+    Name nvarchar(100) NOT NULL UNIQUE check (LEN(Name) > 0),
+);
 
 go
-
-insert into Account (Login, Password, UserId) values
-                                                  ('john.doe', '123456', 1),
-                                                  ('jane.doe', '123456', 2),
-                                                  ('alice.smith', '123456', 3),
-                                                  ('bob.smith', '123456', 4),
-                                                  ('charlie.brown', '123456', 5),
-                                                  ('daisy.brown', '123456', 6);
+create table Faculties(
+                          Id int primary key identity (1,1) not null,
+                          Name nvarchar(100) NOT NULL UNIQUE check (LEN(Name) > 0)
+);
 
 go
+create table Teachers
+(
+    Id int primary key identity (1,1) not null,
+    EmploymentDate date not null check (EmploymentDate <= '1990-01-01'),
+    Name nvarchar(max) NOT NULL check (LEN(Name) > 0),
+    Premium money not null check (Premium >= 0) default 0,
+    Salary money not null check (Salary >= 0),
+    Surname nvarchar(max) NOT NULL check (LEN(Surname) > 0),
+);
 
-select * from Users;
-select * from Account;
-
-select Account.Login, Account.Password, Users.Name+ ' ' + Users.Surname as 'User Info' from Account, Users
-where Account.UserId = Users.Id;
+--inserts
+go
+insert into Groups (Name, Rating, Year) values
+                                            ('A1', 5, 1),
+                                            ('A2', 4, 2),
+                                            ('A3', 3, 3),
+                                            ('A4', 2, 4),
+                                            ('A5', 1, 5);
 
 go
-drop table Account;
-drop table Users;
+insert into Departments (Financing, Name) values
+                                              (100000, 'Department of Mathematics'),
+                                              (200000, 'Department of Physics'),
+                                              (300000, 'Department of Chemistry'),
+                                              (400000, 'Department of Biology'),
+                                              (500000, 'Department of Computer Science');
+
+go
+insert into Faculties (Name) values
+                                 ('Faculty of Mathematics'),
+                                 ('Faculty of Physics'),
+                                 ('Faculty of Chemistry'),
+                                 ('Faculty of Biology'),
+                                 ('Faculty of Computer Science');
+
+go
+insert into Teachers (EmploymentDate, Name, Premium, Salary, Surname) values
+                                                                          ('1980-01-01', 'John', 1000, 10000, 'Doe'),
+                                                                          ('1981-01-01', 'Jane', 2000, 20000, 'Doe'),
+                                                                          ('1982-01-01', 'Alice', 3000, 30000, 'Smith'),
+                                                                          ('1983-01-01', 'Bob', 4000, 40000, 'Smith'),
+                                                                          ('1984-01-01', 'Charlie', 5000, 50000, 'Brown');
+
+--selects
+go
+select * from Groups;
+select * from Departments;
+select * from Faculties;
+select * from Teachers;
+
+select Teachers.Name, Teachers.Surname, Groups.Name, Departments.Name, Faculties.Name  from Teachers, Groups, Departments, Faculties
+where Teachers.Id = Groups.Id and Departments.Id = Groups.Id and Faculties.Id = Groups.Id;
+
+--deletes
+go
+drop table Teachers;
+drop table Faculties;
+drop table Departments;
+drop table Groups;
+
 use master;
-drop database TestDB;
+drop database Academy;
